38331b0fdd690392fec438ddfb17a1b4d7ac6cb3
NaN <121719158@qq.com>
2021-01-14 20:47:12 +0800

    connect4游戏

--- src/main/java/com/reeedking/study/connect4/Connect4TDD.java
+++ src/main/java/com/reeedking/study/connect4/Connect4TDD.java
@@ -1,7 +1,116 @@
 package com.reeedking.study.connect4;
 
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringJoiner;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Connect4TDD {
+
+    private static final int ROWS = 6;
+
+    private static final int COLUMNS = 7;
+
+    private static final String EMPTY = " ";
+
+    private static final String RED = "R";
+
+    private static final String GREEN = "G";
+
+    private static final String DELIMITER = "|";
+
+    private static final int DISCS_TO_WIN = 4;
+
+    private String winner = "";
+
+    private String currentPlayer = RED;
+
+    private String[][] board = new String[ROWS][COLUMNS];
+
+    private PrintStream outputChannel;
+
+    public Connect4TDD(PrintStream printStream) {
+        outputChannel = printStream;
+        for (String[] row : board)
+            Arrays.fill(row, EMPTY);
+    }
+
     public int getNumberOfDiscs() {
-        return 0;
+        return IntStream.range(0, COLUMNS).map(this::getNumberOfDiscsInColumn).sum();
+    }
+
+    private int getNumberOfDiscsInColumn(int column) {
+        //  IntStream.range(0, 7) 返回 0 1 2 3 4 5 6
+        return (int) IntStream.range(0, ROWS).filter(row -> !EMPTY.equals(board[row][column])).count();
+    }
+
+    public int putDiscInColumn(int column) {
+        checkColumn(column);
+        int row = getNumberOfDiscsInColumn(column);
+        checkPositionToInsert(row, column);
+        board[row][column] = currentPlayer;
+        printBoard();
+        checkWinner(row, column);
+        switchPlayer();
+        return row;
+    }
+
+    private void checkColumn(int column) {
+        if (column < 0 || column >= COLUMNS)
+            throw new RuntimeException("Invalid column: " + column);
+    }
+
+    private void checkPositionToInsert(int row, int column) {
+        if (row == ROWS)
+            throw new RuntimeException("No more room in column: " + column);
+    }
+
+    public String getCurrentPlayer() {
+        outputChannel.printf("Player %s turn%n", currentPlayer);
+        return currentPlayer;
+    }
+
+    private void switchPlayer() {
+        if (currentPlayer.equals(RED))
+            currentPlayer = GREEN;
+        else
+            currentPlayer = RED;
+    }
+
+    private void printBoard() {
+        for (int row = ROWS - 1; row >= 0; row--) {
+            StringJoiner stringJoiner = new StringJoiner(DELIMITER, DELIMITER, DELIMITER);
+            Stream.of(board[row]).forEach(stringJoiner::add);
+            outputChannel.println(stringJoiner.toString());
+        }
+    }
+
+    public boolean isFinished() {
+        return getNumberOfDiscs() == ROWS * COLUMNS;
+    }
+
+    private void checkWinner(int row, int column) {
+        if (winner.isEmpty()) {
+//            int startOffset = Math.min(column, row);
+//            int myColumn = column - startOffset,
+//                    myRow = row -startOffset;
+//            StringJoiner stringJoiner = new StringJoiner(" ");
+//            do {
+//                stringJoiner.add(board[myRow++][myColumn++]);
+//            }while (myColumn < COLUMNS && myRow < ROWS);
+//            if (winner)
+            String color = board[row][column];
+            Pattern winPattern = Pattern.compile(".*" + color + "{" + DISCS_TO_WIN + "}.*");
+            String vertical = IntStream.range(0, ROWS).mapToObj(r -> board[r][column]).reduce(String::concat).get();
+            String horizontal = Stream.of(board[row]).reduce(String::concat).get();
+            if (winPattern.matcher(vertical).matches() || winPattern.matcher(horizontal).matches())
+                winner = color;
+        }
+    }
+
+    public String getWinner() {
+        return winner;
     }
 }
--- src/test/java/com/reeedking/study/connect4/ConnectTDDspec.java
+++ src/test/java/com/reeedking/study/connect4/ConnectTDDspec.java
@@ -3,21 +3,147 @@ package com.reeedking.study.connect4;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ConnectTDDspec {
 
     private Connect4TDD tested;
 
+    private OutputStream outputStream;
+
     @BeforeEach
-    public void before(){
-        tested = new Connect4TDD();
+    public void before() {
+        outputStream = new ByteArrayOutputStream();
+        tested = new Connect4TDD(new PrintStream(outputStream));
     }
 
     @Test
-    public void whenGameStartedTheBoardIsEmpty(){
+    public void whenGameStartedTheBoardIsEmpty() {
         assertThat(tested.getNumberOfDiscs(), is(0));
     }
+
+    @Test
+    public void whenDiscOutSideBoardThenRuntimeException() {
+        int column = -1;
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> tested.putDiscInColumn(column));
+        assertEquals(runtimeException.getMessage(), "Invalid column: " + column);
+    }
+
+    @Test
+    public void whenFirstDiscInsertedInColumnThenPositionIsZero() {
+        int column = 1;
+        assertThat(tested.putDiscInColumn(column), is(0));
+    }
+
+    @Test
+    public void whenSecondDiscInsertedInColumnThenPositionIsOne() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.putDiscInColumn(column), is(1));
+    }
+
+    @Test
+    public void whenDiscInsertedThmNumberOfDiscsIncreases() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getNumberOfDiscs(), is(1));
+    }
+
+    @Test
+    public void whenNoMoreRoomInColumnThenRuntimeException() {
+        int column = 1;
+        int maxDiscsInColumn = 6;
+        for (int i = 0; i < maxDiscsInColumn; i++) {
+            tested.putDiscInColumn(column);
+        }
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> tested.putDiscInColumn(column));
+        assertEquals(exception.getMessage(), "No more room in column: " + column);
+    }
+
+    @Test
+    public void whenFirstPlayerPlaysThenDiscColorIsRed() {
+        assertThat(tested.getCurrentPlayer(), is("R"));
+    }
+
+    @Test
+    public void whenSecondPlayerPlaysThenDiscColorIsRed() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getCurrentPlayer(), is("G"));
+    }
+
+    @Test
+    public void whenAskedForCurrentPlayerTheOutputNotice() {
+        tested.getCurrentPlayer();
+        assertThat(outputStream.toString(), containsString("Player R turn"));
+    }
+
+    @Test
+    public void whenADiscIsIntroducedTheBoardIsPrinted() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(outputStream.toString(), containsString("| |R| | | | | |"));
+    }
+
+    @Test
+    public void whenTheGameStartsIsNotFinished() {
+        assertFalse(tested.isFinished(), "The game must not be finished");
+    }
+
+    @Test
+    public void whenNoDiscCanBeIntroducedTheGamesIsFinished() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                tested.putDiscInColumn(j);
+            }
+        }
+        assertTrue(tested.isFinished(), "The game must be finished");
+    }
+
+    @Test
+    public void when4VerticalDiscsAreConnectedThenPlayerWins() {
+        for (int i = 0; i < 3; i++) {
+            tested.putDiscInColumn(1); //R
+            tested.putDiscInColumn(2); //G
+        }
+        assertThat(tested.getWinner(), emptyString());
+        tested.putDiscInColumn(1); //R
+        assertThat(tested.getWinner(), is("R"));
+    }
+
+    @Test
+    public void when4HorizontalDiscAreConnectedThenPlayerWins() {
+        for (int i = 0; i < 3; i++) {
+            tested.putDiscInColumn(i); //R
+            tested.putDiscInColumn(i); //G
+        }
+        assertThat(tested.getWinner(), emptyString());
+        tested.putDiscInColumn(3); //R
+        assertThat(tested.getWinner(), is("R"));
+    }
+
+    @Test
+    public void when4DiagonalDiscsAreConnectedThenThatPlayerWins() {
+        int[] gameplay = new int[]{1, 2, 2, 3, 4, 3, 3, 4, 4, 5, 4};
+        for (int i : gameplay)
+            tested.putDiscInColumn(i);
+        assertThat(tested.getWinner(), is("R"));
+    }
+
+    @Test
+    public void when4Diagonal2DiscsAreConnectedThenThatPlayerWins() {
+        int[] gameplay = new int[]{3, 4, 2, 3, 2, 2, 2, 1, 1, 1, 1};
+        for (int i : gameplay)
+            tested.putDiscInColumn(i);
+        assertThat(tested.getWinner(), is("G"));
+    }
 }
+
+
